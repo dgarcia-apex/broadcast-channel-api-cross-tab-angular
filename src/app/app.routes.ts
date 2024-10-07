dc564cfd42a914362818a37e4242f1e80ed4f0e4
David Garcia <david.garcia@wachter.com>
2024-10-07 13:51:02 -0600

    initial

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { PageOneComponent } from './page-one/page-one.component';
+import { PageTwoComponent } from './page-two/page-two.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'page-one',
+    loadComponent: () =>
+      import('./page-one/page-one.component').then((m) => m.PageOneComponent),
+  },
+  {
+    path: 'page-two',
+    loadComponent: () =>
+      import('./page-two/page-two.component').then((m) => m.PageTwoComponent),
+  },
+];
